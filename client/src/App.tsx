a4f1e11b35532f4c6412396eb5bd1c256597f2b4
dogukanakkaya <doguakkaya27@hotmail.com>
2021-11-21 20:36:53 +0300

    refactor: move InnerApp.tsx to App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,32 @@
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './hooks/useAuth'
-import InnerApp from './InnerApp'
+
+const Dashboard = React.lazy(() => import('./pages/Dashboard'))
+const Login = React.lazy(() => import('./pages/Login'))
 
 function App() {
     return (
         <BrowserRouter>
             <AuthProvider>
-                <InnerApp />
+                <Routes>
+                    <Route
+                        path="/login"
+                        element={
+                            <React.Suspense fallback={<>...</>}>
+                                <Login />
+                            </React.Suspense>
+                        }
+                    />
+                    <Route
+                        path="/"
+                        element={
+                            <React.Suspense fallback={<>...</>}>
+                                <Dashboard />
+                            </React.Suspense>
+                        }
+                    />
+                </Routes>
             </AuthProvider>
         </BrowserRouter>
     )
