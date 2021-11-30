c5a6bcd23019020be17005a3446906955df514cf
dogukanakkaya <doguakkaya27@hotmail.com>
2021-11-30 20:07:22 +0300

    feat: create collection graphql mutations

--- server/src/graphql/schema.ts
+++ server/src/graphql/schema.ts
@@ -1,11 +1,12 @@
-import { GraphQLSchema, GraphQLObjectType, GraphQLList, GraphQLID } from 'graphql'
+import { GraphQLSchema, GraphQLObjectType, GraphQLList, GraphQLID, GraphQLNonNull, GraphQLString } from 'graphql'
+import { HydratedDocument } from 'mongoose'
 
 // Database
-import { Collection } from '../models/Collection'
+import { Collection, ICollection } from '../models/Collection'
 import { Request } from '../models/Request'
 
 // Types
-import { CollectionType } from './types/collection'
+import { CollectionInputType, CollectionType } from './types/collection'
 import { RequestType } from './types/request'
 
 const RootQuery = new GraphQLObjectType({
@@ -13,8 +14,9 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         collections: {
             type: new GraphQLList(CollectionType),
-            async resolve(parent, args) {
-                return await Collection.find().populate('requests')
+            resolve(_, args) {
+                return Collection.find().populate('requests')
+                //return await Collection.find()
             }
         },
         collection: {
@@ -22,8 +24,9 @@ const RootQuery = new GraphQLObjectType({
             args: {
                 _id: { type: GraphQLID }
             },
-            async resolve(parent, { _id }) {
-                return await Collection.findById(_id).populate('requests')
+            resolve(_, { _id }) {
+                return Collection.findById(_id).populate('requests')
+                //return await Collection.findById(_id)
             }
         },
         request: {
@@ -31,13 +34,56 @@ const RootQuery = new GraphQLObjectType({
             args: {
                 _id: { type: GraphQLID }
             },
-            async resolve(parent, { _id }) {
-                return await Request.findById(_id)
+            resolve(_, { _id }) {
+                return Request.findById(_id)
+            }
+        }
+    }
+})
+
+const RootMutation = new GraphQLObjectType({
+    name: 'RootMutation',
+    fields: {
+        createCollection: {
+            type: CollectionType,
+            args: {
+                input: { type: CollectionInputType }
+            },
+            resolve(_, args) {
+                const { title }: ICollection = args.input
+
+                const collection: HydratedDocument<ICollection> = new Collection({
+                    title
+                })
+
+                return collection.save()
+            }
+        },
+        updateCollection: {
+            type: CollectionType,
+            args: {
+                _id: { type: GraphQLID },
+                input: { type: CollectionInputType }
+            },
+            resolve(_, args) {
+                const { title }: ICollection = args.input
+
+                return Collection.findByIdAndUpdate(args._id, { title }).populate('requests')
+            }
+        },
+        deleteCollection: {
+            type: CollectionType,
+            args: {
+                _id: { type: GraphQLID }
+            },
+            resolve(_, args) {
+                return Collection.findByIdAndDelete(args._id)
             }
         }
     }
 })
 
 export default new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation: RootMutation
 })
--- server/src/graphql/types/collection.ts
+++ server/src/graphql/types/collection.ts
@@ -1,11 +1,33 @@
-import { GraphQLObjectType, GraphQLList, GraphQLID, GraphQLString } from 'graphql'
+import { GraphQLObjectType, GraphQLList, GraphQLID, GraphQLString, GraphQLInputObjectType, GraphQLNonNull } from 'graphql'
+import { Request } from '../../models/Request';
 import { RequestType } from './request'
 
 export const CollectionType = new GraphQLObjectType({
     name: 'Collection',
     fields: {
         _id: { type: GraphQLID },
-        title: { type: GraphQLString },
+        title: { type: new GraphQLNonNull(GraphQLString) },
         requests: { type: new GraphQLList(RequestType) }
+        /*
+        requests: {
+            type: new GraphQLList(RequestType),
+            resolve(parent) {
+                return Request.find({
+                    _id: {
+                        $in: parent.requests
+                    }
+                })
+                console.log(parent);
+
+            }
+        }
+        */
+    }
+})
+
+export const CollectionInputType = new GraphQLInputObjectType({
+    name: 'CollectionInput',
+    fields: {
+        title: { type: new GraphQLNonNull(GraphQLString) },
     }
 })
